00c82f8fa0ff223448d1b3617f4f8e23d594d6b0
Slinging <130846147+Slinging@users.noreply.github.com>
2024-10-10 21:33:34 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -35,6 +35,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         document.getElementById('login-btn').style.display = 'block';
         document.querySelector('.user-container').style.visibility = 'hidden';
         document.querySelector('.user-container').style.opacity = '0';
+        window.location.href = '/login'; // 如果没有登录信息，跳转到登录页面*/
     } else {
         // 有 token 时获取用户信息
         try {
